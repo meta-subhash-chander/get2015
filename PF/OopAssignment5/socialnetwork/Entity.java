2e61168b32ff19c7ff5df0adde3c5c264b477017
Subhash Chander <subhash.chander@metacube.com>
2015-08-24 19:41:55 +0530

    OopAssignment5

---
+++ PF/OopAssignment5/socialnetwork/Entity.java
@@ -0,0 +1,23 @@
+
+ 
+package socialnetwork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author subahsh chander
+ */
+public class Entity {
+    String email;
+    String name;
+    ArrayList<String> friends=new ArrayList<String>();
+    public Entity(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+    
+
+    
+    }
