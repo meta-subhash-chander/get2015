5479b2fe7ccb9fd22d6bc3671e067e1bd6b37c35
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-07 17:02:49 +0530

    Update Node.java

--- Advance-DSA/List/Node.java
+++ Advance-DSA/List/Node.java
@@ -6,7 +6,7 @@ Revision Log    : 2015-10-05: Subhash Chander : created.
 Use             : This class is used to perform all opertion of list
                 :
  ****************************************************************************************/
-public class Node {
+public class Node {  
 
 	Node next;
 	Object value;
