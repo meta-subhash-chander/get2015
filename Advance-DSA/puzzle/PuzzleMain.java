e1043816563f78395abdb8b512ac4a8297f9d994
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-08 10:20:36 +0530

    Create PuzzleMain.java

---
+++ Advance-DSA/puzzle/PuzzleMain.java
@@ -0,0 +1,32 @@
+/**
+ * This class contains  function call of
+ * a sliding puzzle.
+ *
+ * @author subhash chander
+ */
+package puzzle;
+
+public class PuzzleMain {
+	public static void main(String[] args) {
+	    int[] input;
+	    Puzzle puzzle = null;
+
+	    // Retrieve input based on argument length.
+	    if (args.length == 0){
+	      puzzle = new Puzzle(Puzzle.getConsoleInput());
+	    } else {
+	      input = Puzzle.readFromFile(args[0]);
+	      puzzle = (args.length == 1) ? new Puzzle(input) : new Puzzle(input, args[1]);
+	    }
+
+	    // Check if the puzzle is solvable.
+	    if (!puzzle.isSolvable()) {
+	      System.out.printf("Given puzzle:%s\n\nis NOT solvable!\n", puzzle.state.toString());
+	      System.exit(0);
+	    }
+
+	    // Solve the puzzle.
+	    puzzle.solve();
+	  }
+
+}
