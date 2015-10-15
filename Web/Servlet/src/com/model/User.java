690426603d3ae21136e686d9af2b0b6c6317cadd
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-15 15:09:47 +0530

    Update User.java

--- Web/Servlet/src/com/model/User.java
+++ Web/Servlet/src/com/model/User.java
@@ -1,24 +1,62 @@
+/****************************************************************************************
+Name            : User
+Revision Log    : 2015-10-13: Subhash Chander : created.
+                : 
+                : 
+Use             : This class have user attribute to create account
+                :
+ ****************************************************************************************/
 package com.model;
 
 public class User {
 	private String username;
 	private String email;
 	private String password;
+	/**
+	 * getUsername method :method is use to get user name
+	 * 
+	 * @return : user name
+	 */
 	public String getUsername() {
 		return username;
 	}
+	/**
+	 * setUsername method :method is use to set user name
+	 * 
+	 * @param :user name
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	/**
+	 * getEmail method :method is use to get user Email
+	 * 
+	 * @return : user Email
+	 */
 	public String getEmail() {
 		return email;
 	}
+	/**
+	 * setEmail method :method is use to set user email
+	 * 
+	 * @param :email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	/**
+	 * getPassword method :method is use to get user password
+	 * 
+	 * @return : user password
+	 */
 	public String getPassword() {
 		return password;
 	}
+	/**
+	 * setEmail method :method is use to set user password
+	 * 
+	 * @param :password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
