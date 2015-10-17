645db2c03d3292284c80014b5e3098fc7778dce9
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-17 13:46:17 +0530

    Update DoPrepareForm.java

--- Web/mvcassignment1/src/com/servlets/DoPrepareForm.java
+++ Web/mvcassignment1/src/com/servlets/DoPrepareForm.java
@@ -1,9 +1,16 @@
+/****************************************************************************************
+Name            : DoPrepareForm
+Revision Log    : 2015-10-13: Subhash Chander : created.
+                : 
+                : 
+Use             : This class is used to  PrepareForm of employee
+                :
+ ****************************************************************************************/
 package com.servlets;
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,49 +18,36 @@ import javax.servlet.http.HttpServletResponse;
 import com.helper.EmployeeHelper;
 import com.model.Employee;
 
-/**
- * Servlet implementation class DoPrepareForm
- */
-
 public class DoPrepareForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public DoPrepareForm() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * doGet method : used to create the employee data
+	 * 
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 
 		int id = Integer.parseInt(request.getParameter("id"));
-		 if (id != -1) {             
+		if (id != -1) {
 			Employee employee = EmployeeHelper.getEmployee(id);
 			request.setAttribute("employee", employee);
-			 request.setAttribute("id", id);
+			request.setAttribute("id", id);
 
 		}
-       
+
 		request.getRequestDispatcher("jsp/formPage.jsp").forward(request,
 				response);
 
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * doPost method : used to call get method
+	 * 
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
 		doGet(request, response);
 
 	}
