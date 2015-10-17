05cec03f5cb7f7f2088f2ec584ab1afb1bdfe522
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-17 13:46:54 +0530

    Update DoRegister.java

--- Web/mvcassignment1/src/com/servlets/DoRegister.java
+++ Web/mvcassignment1/src/com/servlets/DoRegister.java
@@ -1,3 +1,11 @@
+/****************************************************************************************
+Name            : DoRegister
+Revision Log    : 2015-10-13: Subhash Chander : created.
+                : 
+                : 
+Use             : This class is used to  control register user
+                :
+ ****************************************************************************************/
 package com.servlets;
 
 import java.io.IOException;
@@ -6,7 +14,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,44 +21,37 @@ import javax.servlet.http.HttpServletResponse;
 import com.helper.EmployeeHelper;
 import com.model.Employee;
 
-/**
- * Servlet implementation class DoRegister
- */
 public class DoRegister extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DoRegister() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * doGet method : used to create the employee data
+	 * 
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	 int employId=Integer.parseInt(request.getParameter("employId"));
-	 String name= request.getParameter("name");
-	 String email=request.getParameter("email");
-	 int age=Integer.parseInt(request.getParameter("age"));
-	 //String date=request.getParameter("date");
-	 DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-	 Date date = new Date();
-	 Employee employee= new Employee(employId, name, email, age, dateFormat.format(date));
-	 EmployeeHelper.addInList(employee);
-	 request.getRequestDispatcher("/ShowList").forward(request, response);
-	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		int employId = Integer.parseInt(request.getParameter("employId"));
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		int age = Integer.parseInt(request.getParameter("age"));
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Date date = new Date();
+		Employee employee = new Employee(employId, name, email, age,
+				dateFormat.format(date));
+		EmployeeHelper.addInList(employee, employId);
+		request.getRequestDispatcher("/ShowList").forward(request, response);
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * doPost method : used to call get method
+	 * 
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	doGet(request, response);
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		doGet(request, response);
 	}
 
 }
