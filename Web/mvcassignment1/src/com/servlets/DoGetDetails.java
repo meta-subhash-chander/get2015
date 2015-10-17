79e96d32eb69581fa95657ce3fe9b86325f03836
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-17 13:45:54 +0530

    Update DoGetDetails.java

--- Web/mvcassignment1/src/com/servlets/DoGetDetails.java
+++ Web/mvcassignment1/src/com/servlets/DoGetDetails.java
@@ -1,9 +1,16 @@
+/****************************************************************************************
+Name            : DoGetDetails
+Revision Log    : 2015-10-13: Subhash Chander : created.
+                : 
+                : 
+Use             : This class is used to get the employee detail
+                :
+ ****************************************************************************************/
 package com.servlets;
 
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,39 +18,32 @@ import javax.servlet.http.HttpServletResponse;
 import com.helper.EmployeeHelper;
 import com.model.Employee;
 
-/**
- * Servlet implementation class GoGetDetails
- */
 public class DoGetDetails extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DoGetDetails() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * doGet method : used to send employee detail to jsp page
+	 * 
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	
-	  int id= Integer.parseInt(request.getParameter("id"));
-	  Employee employee= EmployeeHelper.getEmployee(id);
-	  request.setAttribute("employee", employee);
-	   request.getRequestDispatcher("jsp/employeeDetails.jsp").forward(request, response);
-		
-	
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		int id = Integer.parseInt(request.getParameter("id"));
+		Employee employee = EmployeeHelper.getEmployee(id);
+		request.setAttribute("employee", employee);
+		request.getRequestDispatcher("jsp/employeeDetails.jsp").forward(
+				request, response);
+
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	
-	  doPost(request, response);
+	/**
+	 * doPost method : used to call get method
+	 * 
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);
 	}
 
 }
