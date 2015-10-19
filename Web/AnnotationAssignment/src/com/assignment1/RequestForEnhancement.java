806a523c839b2fcf9d4942583a21c921aa96bc17
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-19 16:37:14 +0530

    Update RequestForEnhancement.java

--- Web/AnnotationAssignment/src/com/assignment1/RequestForEnhancement.java
+++ Web/AnnotationAssignment/src/com/assignment1/RequestForEnhancement.java
@@ -1,3 +1,11 @@
+/****************************************************************************************
+Name            : RequestForEnhancement
+Revision Log    : 2015-10-16: Subhash Chander : created.
+                : 
+                : 
+Use             : Annotation RequestForEnhancement request with elements id, synopsis, 
+                : engineer, and date. Specify the default value as unassigned for engineer and unknown for date.             
+ ****************************************************************************************/
 package com.assignment1;
 
 public @interface RequestForEnhancement {
@@ -12,3 +20,4 @@ public @interface RequestForEnhancement {
 
 }
 
+                                                                                                                                          
