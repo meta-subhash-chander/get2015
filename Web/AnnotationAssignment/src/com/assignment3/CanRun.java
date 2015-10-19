543176fe4f34bda9c9d99e44dd0070d5d73b7bc4
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-19 16:37:39 +0530

    Update CanRun.java

--- Web/AnnotationAssignment/src/com/assignment3/CanRun.java
+++ Web/AnnotationAssignment/src/com/assignment3/CanRun.java
@@ -1,10 +1,14 @@
-
+/****************************************************************************************
+Name            : CanRun
+Revision Log    : 2015-10-16: Subhash Chander : created.
+                : 
+                : 
+Use             : Annotation RequestForEnhancement request with no element             
+ ****************************************************************************************/
 package com.assignment3;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
-
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CanRun {
 	
