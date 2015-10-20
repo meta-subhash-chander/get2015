84c003f1b97944f0e475ce225eba9df85aa6d154
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-20 14:22:54 +0530

    Update EmployeeMain.java

--- Web/AnnotationAssignment/src/com/assignment2/EmployeeMain.java
+++ Web/AnnotationAssignment/src/com/assignment2/EmployeeMain.java
@@ -7,7 +7,9 @@ Use             :Jackson API to parse a Model class to JSON and vice versa
  ****************************************************************************************/
 package com.assignment2;
 
+import java.io.File;
 import java.io.IOException;
+
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class Main {
@@ -23,15 +25,13 @@ public class Main {
 		employee1.setEmailId("cs@gmail.com");
 		employee1.setDateOfJoining("03/08/2015");
 		System.out.println(employee1.toString());
-		String json = "{\"employeeName\":\"Subhash\",\"employeeId\":101,\"emailId\":\"sc@gmail.com\",\"dateOfBirth\":\"02/02/1995\",\"dateOfJoining\":\"03/08/2015\",\"ctcPerAnnum\":1.0E8}";
-		Employee employee2;
+	    Employee employee2;
 
 		try {
-			System.out.println(mapper.writeValueAsString(employee1));
-
-			System.out.println("Json file = " + json);
-			employee2 = mapper.readValue(json, Employee.class);
-			System.out.println(employee2.toString());
+			mapper.writeValue(new File("C:\\Users\\Subhash\\workspace1\\AnnotationAssignment1\\src\\com\\assignment2\\employee.json"),employee1);	
+		    employee2 = mapper.readValue(
+					new File("C:\\Users\\Subhash\\workspace1\\AnnotationAssignment1\\src\\com\\assignment2\\employeeJsonFromEmployeeModel.json"),Employee.class);
+	System.out.println(employee2.toString());
 
 		} catch (IOException e) {
 			e.printStackTrace();
