acdac13439de2dfda32e0cbcc649ba5a2e44f7a3
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-27 17:21:30 +0530

    Update LogoutController.java

--- Web/CaseStudyAssignment4/src/com/controller/LogoutController.java
+++ Web/CaseStudyAssignment4/src/com/controller/LogoutController.java
@@ -1,3 +1,10 @@
+/****************************************************************************************
+Name            : LogoutController
+Revision Log    : 2015-10-25: Subhash Chander : created.
+                : 
+                : 
+Use             : This class is used to control   admin  logout            
+ ****************************************************************************************/
 package com.controller;
 
 import java.io.IOException;
@@ -15,29 +22,34 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/LogoutController")
 public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogoutController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		HttpSession session = request.getSession();
-        session.setAttribute("email", null);
-		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+	public LogoutController() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * doGet method : delete the session and redirect to index.jsp page
+	 * 
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		session.setAttribute("email", null);
+		getServletContext().getRequestDispatcher("/index.jsp").forward(request,
+				response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
