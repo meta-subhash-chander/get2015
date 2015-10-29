9e12e02dccdbcd45dddd6d7e8144f23cc1500893
meta-subhash-chander <subhash.chander@metacube.com>
2015-10-29 18:55:07 +0530

    Update script.js

--- Web/JquerySession1/Assignment5/js/script.js
+++ Web/JquerySession1/Assignment5/js/script.js
@@ -1,7 +1,9 @@
  
 $(document).ready(function() {
-	$("p").siblings().css({
-		"color" : "black",
-		"border" : "1px dashed red"
+	$("p").siblings("h1").css({
+		"color" : "red",
+		"border" : "2px dashed green"
 	});
+	$("p").parents().parentsUntil("html").css({"border": "2px solid blue"});
+	 $("p").next().css({"border": "2px solid pink"});
 });
