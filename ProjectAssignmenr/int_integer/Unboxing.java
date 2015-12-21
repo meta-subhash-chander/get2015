00fe441893cc91f16cc2058cd6f0b1fcff77c944
Subhash Chander <subhash.chander@metacube.com>
2015-12-21 18:12:19 +0530

    ProjectAssignment

---
+++ ProjectAssignmenr/int_integer/Unboxing.java
@@ -0,0 +1,10 @@
+package int_integer;
+class Unboxing{  
+  public static void main(String args[]){  
+    Integer i=new Integer(50);  
+        int a=i;  
+      //Autoboxing :integer object to  primitive
+        System.out.println(a);  
+ }   
+}  
+      
